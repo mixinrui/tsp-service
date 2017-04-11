4d2dc5447d9e6a751d67e869cafa2cb93b4c6ff5
mk <331915933@qq.com>
2017-04-11 21:15:02 +0800

    init project

---
+++ src/main/java/com/boxfish/tsp/entity/Test.java
@@ -0,0 +1,7 @@
+package com.boxfish.tsp.entity;
+
+/**
+ * Created by mk on 17/4/11.
+ */
+public class Test {
+}
---
+++ src/main/java/com/boxfish/tsp/repository/Test.java
@@ -0,0 +1,7 @@
+package com.boxfish.tsp.repository;
+
+/**
+ * Created by mk on 17/4/11.
+ */
+public class Test {
+}
